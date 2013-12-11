c15b6396fed8caf3f969cef8a4b85de241ee3eef
Igor Antonov <keritaf@gmail.com>
2013-12-11 16:30:08 +0300

    Fix lab4

--- 4/lab4.rb
+++ 4/lab4.rb
@@ -3,20 +3,21 @@ require 'pry'
 require 'colorize'
 require 'matrix'
 
-class Fixnum
-  def f
-    val = 1
-    self.downto(2) do |k|
-      val = val * k
-    end
-    val
+class Integer
+  def fact
+    (1..self).inject(:*) || 1
   end
 end
 
-N = 4
+def permutations from, by
+  from.fact/(by.fact*((from-by).fact))
+end
 
-generated = [1, Array.new(N) { rand(1..100) > 50 ? 0 : 1 }].flatten
-polynom = Polynom.new(generated)#new([1,1,0,0,1])#
+N = 6
+
+generated = [1, Array.new(N-1) { [0,1].sample }, 1].flatten
+#polynom = Polynom.new([1, 0, 1, 0, 0, 1, 1, 0,1])
+polynom = Polynom.new(generated)
 
 lsrf = Lsrf.new(polynom)
 
@@ -28,10 +29,12 @@ loop do
   states << lsrf.state
   puts lsrf.state.inspect
   lsrf.next
-  break if states.include?(lsrf.state)
+  break if states.first == lsrf.state or states.count > 2**N
 end
 
 T = Array.new(2**N) { Array.new(2**N) { 0 } }
+
+#binding.pry
 
 states.zip(states[1..-1]).each do |prv, nxt|
   unless nxt.nil?
@@ -45,17 +48,17 @@ C = Array.new(2**N) do |i|
     if j>i
       0
     else
-      (i.f / (j.f * ( i-j ).f)) % 2
+      (permutations i,j) % 2
     end
   end
 end
 
-t = Matrix[*T].transpose
+t = Matrix[*T]
 
 puts "T".cyan
 
 t.to_a.each do |arr|
-  puts arr.inspect
+  puts arr.join
 end
 
 c = Matrix[*C].transpose
@@ -68,7 +71,7 @@ string = l.to_a[str].map {|i| i % 2 }
 
 puts "L row: ".cyan
 
-puts string.inspect
+puts string.join
 
 twos = Array.new(1000) {|i| 2 ** i }
 
@@ -79,6 +82,8 @@ string.each_with_index do |v, i|
   end
 end
 
+puts
+puts "Polynom: #{polynom.to_s.cyan}"
 if sum == 0
   puts "Linear".green
 else
